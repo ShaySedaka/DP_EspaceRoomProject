88d1bbeea4ad0e8363bb4e65998f4fd310c0fe64
Shay Sedaka <shays@justplay.lol>
2021-12-18 19:15:55 +0200

    First Push (Camera Manager Infastructure)

---
+++ EscapeRoomDesignPatterns/Assets/Scripts/Camera Management/CameraState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CameraState
+{
+    Transform _currentCameraTranform;
+    CameraState _rightSideState, _leftSideState;
+
+    public Transform CurrentCameraTranform { get => _currentCameraTranform; set => _currentCameraTranform = value; }
+    public CameraState RightSideState { get => _rightSideState; set => _rightSideState = value; }
+    public CameraState LeftSideState { get => _leftSideState; set => _leftSideState = value; }
+
+}
---
+++ EscapeRoomDesignPatterns/Assets/Scripts/Camera Management/CameraStateManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStateManager : MonoBehaviour
+{
+    [SerializeField] List<Transform> _cameraAngles;
+    [SerializeField] CameraState _currentCameraState;
+    [SerializeField] Camera _playerCamera;
+
+    private List<CameraState> _RoomCameraStates = new List<CameraState>();
+
+    void Start()
+    {
+        InitializeCameraState();
+        _currentCameraState = _RoomCameraStates[0];
+        MoveCameraToCurrentTranform();
+    }
+
+
+    public void LookRight()
+    {
+        _currentCameraState = _currentCameraState.RightSideState;
+        MoveCameraToCurrentTranform();
+
+    }
+
+    public void LookLeft()
+    {
+        _currentCameraState = _currentCameraState.LeftSideState;
+        MoveCameraToCurrentTranform();
+    }
+
+    private void InitializeCameraState()
+    {
+        // creating the base CameraState object and adding them to the room state list
+        for (int i = 0; i < _cameraAngles.Count; i++)
+        {
+            CameraState cs = new CameraState();
+            cs.CurrentCameraTranform = _cameraAngles[i];
+            _RoomCameraStates.Add(cs);
+        }
+
+        // connecting the state in the room state list cyclicaly 
+        for (int i = 0; i < _RoomCameraStates.Count; i++)
+        {
+            CameraState cs = _RoomCameraStates[i];
+            cs.LeftSideState = _RoomCameraStates[(i - 1 >= 0)? (i-1) : (i-1+_RoomCameraStates.Count) ];
+            cs.RightSideState = _RoomCameraStates[(i + 1) % (_RoomCameraStates.Count)];
+        }
+    }
+
+    private void MoveCameraToCurrentTranform()
+    {
+        //turning the camera
+        _playerCamera.transform.position = _currentCameraState.CurrentCameraTranform.position;
+        _playerCamera.transform.rotation = _currentCameraState.CurrentCameraTranform.rotation;
+    }
+
+}
