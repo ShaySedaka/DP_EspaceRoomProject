92c0db043cfb42f7cf0b579ffa92d6dd89f5aefd
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2021-12-23 19:52:51 +0200

    Combining two items is now possible, but only when the cursor has no image following it
    Combining is done through ItemIcon, and the parameters are taken from ItemSO... kinda un-clean?

---
+++ EscapeRoomDesignPatterns/Assets/Scripts/Interactibles/Abstacts/Combanieable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combanieable : Interactible
+{
+    [SerializeField] private string _itemToCombineWith;
+
+    [SerializeField] private ItemSO _itemCombo;
+
+    public override void Interact()
+    {
+        if(PlayerInventory.Instance.SelectedItem.name.Equals(_itemToCombineWith))
+        {
+            PlayerInventory.Instance.SelectedItem.RemoveItemFromInventory();
+            RemoveThisItem();
+        }
+
+        _itemCombo.AddItemToInventory();
+    }
+
+    private void RemoveThisItem()
+    {
+        gameObject.GetComponent<ItemIcon>().ItemSORef.RemoveItemFromInventory();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
